7c643309d8700ee94c84a67237ab30429cce152e
alvincf10 <alvinwijaya446@gmail.com>
2021-02-21 22:26:38 +0700

    menambahkan text input

--- screens/Registrasionscreen.jsx
+++ screens/Registrasionscreen.jsx
@@ -7,33 +7,28 @@ import {
 } from 'react-native';
 
 import { LinearGradient } from 'expo-linear-gradient';
-import { SearchableDropdown } from 'react-native-searchable-dropdown';
 
 export default function Registrasionscreen({ route, navigation }) {
     const { username } = route.params;
 
-    const items = [
-        //name key is must.It is to show the text in front
-        { id: 1, name: 'angellist' },
-        { id: 2, name: 'codepen' },
-        { id: 3, name: 'envelope' },
-    ];
-
     return (
         <View style={{ flex: 1, marginTop: 25 }}>
-            <LinearGradient style={{ width: '100%', height: 30 }} start={[0, 1]} end={[1, 1]} colors={['rgba(197,52,21,1)', "rgba(211,102,19,1)"]}>
-                <StatusBar translucent backgroundColor="transparent" />
-                <Text style={{ color: 'white', marginLeft: '13%' }} >
+            <LinearGradient style={{ width: '100%', height: 40 }} start={[0, 1]} end={[1, 1]} colors={['rgba(197,52,21,1)', "rgba(211,102,19,1)"]}>
+                <Text style={{ fontSize: 18, color: 'white', marginLeft: '10%', margin: 5 }} >
                     Phone Number
-                        </Text>
+                </Text>
+                <StatusBar translucent Color="transparent" />
             </LinearGradient>
 
             <View style={{ flexDirection: 'row' }}>
 
-
+                <TextInput
+                    style={{ width: '10%', height: 50, marginTop: 20, marginLeft: '6%', borderBottomWidth: 1, }}
+                    placeholder="+62"
+                />
 
                 <TextInput
-                    style={{ width: '65%', height: 50, marginTop: 20, marginLeft: '30%', borderBottomWidth: 1, }}
+                    style={{ width: '75%', height: 50, marginTop: 20, marginLeft: '10%', borderBottomWidth: 1, borderBottomColor: 'rgba(197,52,21,1)' }}
                     placeholder="Enter Your Number"
                 />
             </View>
